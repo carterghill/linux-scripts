b066794ef2ac71eaa861feffd7e36be2176cbe3e
Carter Hill <carterghill@gmail.com>
2019-11-24 21:00:01 -0600

    brightness.c syntax changed to get rid of warnings on compilation

--- brightness.c
+++ brightness.c
@@ -6,52 +6,38 @@
 
 int main(int argc, char *argv[]) {
 
+	int num;
+	char buf[64];
+	char strNum[64];
+	char cmd[256];
+
+	FILE *ls;
+
 	/* Check for argument, output and exit if it's not there */
 	if (argc <= 1) {
 		printf("Usage: brightness [number]\n");
 		return 1;
 	}
 
-	/* Check if file exists, create it if not */
-	/*if( access(".brightness", F_OK ) != -1 ) {
-		//file exists
-	} else {
-		//file doesn't exist
-		printf("not there\n");
-		system("echo 5 > .brightness");
-	}*/
-
 	/* Get brightness level from file */
-	FILE *ls = popen("cat /sys/class/backlight/intel_backlight/brightness",
-		       	"r");
-	char buf[64];
-	int num;
-
-	while (fgets(buf, sizeof(buf), ls) != 0) {
-		printf("buf: %s\n", buf);
-		//strcpy(files[i], buf);
-	}
+	ls = popen("cat /sys/class/backlight/intel_backlight/brightness", "r");
+	while (fgets(buf, sizeof(buf), ls) != 0) {}
 	pclose(ls);
+	
+	/* Turn the brightness into an integer */
 	num = atoi(buf);
-	printf("cur nrightness: %i\n", num);
 
-	char strNum[80] = {};
+	/* Add the program argument as an integer */
 	num = num + atoi(argv[1]);
-	sprintf(strNum, "%i", num);
-	printf("new brightness: %s\n", strNum);
 
-	/* Set new brightness level with xrandr */
-	char cmd[256];
+	/* Store the number as a string */
+	sprintf(strNum, "%i", num);
+
+	/* Set up the command string echo strNum > file*/
 	strcpy(cmd, "echo ");
 	strcat(cmd, strNum);
 	strcat(cmd, " > /sys/class/backlight/intel_backlight/brightness");
 	system(cmd);
-	
-	/* update current brightness */
-	/*strcpy(cmd, "echo ");
-	strcat(cmd, strNum);
-	strcat(cmd, " > .brightness");
-	system(cmd);*/
 
 	return 1;
 
