6029ce0baa81d3d7d9f73bb9845ed9f4e95d7382
Carter Hill <carterghill@gmail.com>
2019-11-25 14:47:06 -0600

    Updated auto background to take in two arguments, one for folder path and one for interval

--- auto_background.c
+++ auto_background.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>	
+#include <unistd.h> 
 
 int main(int argc, char *argv[]) {
 	
-	FILE *ls = popen("ls ~/Pictures/Wallpapers/", "r");
+	FILE *ls;
 	char buf[256];
 	char files[64][64];
 	int i = 0;
 	int num;
-	char command[256];
 
+	/* User must specify path and time interval */
+	if (argc < 3) {
+		printf("Usage: auto_background [path] [time]\n");
+		return 1;
+	}
+
+	/* List the files in the folder */
+	strcpy(buf, "ls ");
+	strcat(buf, argv[1]);
+	ls = popen(buf, "r");
+
+	/* Loop through the list and store each image path as a string */
 	while (fgets(buf, sizeof(buf), ls) != 0) {
-		printf("buf: %s\n", buf);
 		strcpy(files[i], buf);
 		i++;
 	}
 	pclose(ls);
+	
+	/* Loop forever and switch to a new background then sleep */
 	num = i;
-
 	for (i = 0; ; i++) {
-		strcpy(command, "feh --bg-scale ~/Pictures/Wallpapers/");
-		strcat(command, files[i]);
-		printf("Changing to %i. %s\n", i, files[i]);
-		system(command);
-		sleep(2);
+		strcpy(buf, "feh --bg-scale ");
+		strcat(buf, argv[1]);
+		strcat(buf, "/");
+		strcat(buf, files[i]);
+		system(buf);
+		sleep(atoi(argv[2]));
 		if (i >= num-1) {
-			printf("%i >= %i\n", i, num);
 			i = -1;
 		}
 	}
